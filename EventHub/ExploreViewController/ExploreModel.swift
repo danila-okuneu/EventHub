feb4f15f695d40d192602cd34f5334afe334019d
Igor Guryan <sacujo93@yandex.ru>
2024-11-17 23:52:11 +0300

    add files

---
+++ EventHub/ExploreViewController/ExploreModel.swift
@@ -0,0 +1,15 @@
+
+//
+//  ExploreModel.swift
+//  EventHub
+//
+//  Created by Igor Guryan on 17.11.2024.
+//
+
+import Foundation
+
+enum ExploreSection: Int, Hashable, CaseIterable {
+    case categories
+    case upcoming
+    case nearby
+}
