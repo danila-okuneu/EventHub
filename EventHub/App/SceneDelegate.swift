a2f35ea161bb77864bcbea68b8dc62204d6217eb
Danila Okuneu <accounts@okuneu.com>
2024-11-19 16:04:07 +0300

    [FEAT] Register Screen

--- EventHub/App/SceneDelegate.swift
+++ EventHub/App/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
 		window?.backgroundColor = .appPurpleDark
 		window?.windowScene = windowScene
-        window?.rootViewController = CustomTabBarController()
+		window?.rootViewController = RegisterViewController()
 		window?.makeKeyAndVisible()
 	}
 
