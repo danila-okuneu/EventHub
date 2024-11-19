71e296fff53b9ced53610c2aa4c21bf2d9b63c3a
Nadezhda <el.shtolts@yandex.ru>
2024-11-20 02:15:48 +0300

    created favourites screen with mock date + empty screen

--- EventHub/FavouritesScreen/EventCollectionViewCell.swift
+++ EventHub/FavouritesScreen/EventCollectionViewCell.swift
@@ -14,10 +14,13 @@ class EventCollectionViewCell: UICollectionViewCell {
     private let eventImageView = UIImageView()
     private let dateLabel = UILabel()
     private let titleLabel = UILabel()
-    private let placeLabel = UILabel()
+
+    private let locationImageView = UIImageView()
+    private let locationLabel = UILabel()
+
     private let bookmarkButton = UIButton(type: .system)
     
-    private var isBookmarked = false
+    private var isBookmarked = true
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,55 +32,89 @@ class EventCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupViews() {
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 10
+        
         eventImageView.contentMode = .scaleAspectFill
         eventImageView.clipsToBounds = true
+        eventImageView.layer.cornerRadius = 10
         
-        dateLabel.font = UIFont.systemFont(ofSize: 14)
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        placeLabel.font = UIFont.systemFont(ofSize: 14)
         
-        bookmarkButton.setTitle("⭐️", for: .normal)
+        dateLabel.font = UIFont.cerealFont(ofSize: 13, weight: .light)
+        dateLabel.textColor = .accent
+        
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        titleLabel.numberOfLines = 0
+        
+        locationLabel.font = UIFont.cerealFont(ofSize: 13)
+        locationLabel.textColor = .appGrayTabbar
+        locationLabel.numberOfLines = 0
+        locationImageView.image = .location
+        
+        bookmarkButton.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+        bookmarkButton.tintColor = UIColor.appRed
+
         bookmarkButton.addTarget(self, action: #selector(bookmarkTapped), for: .touchUpInside)
         
-        let stackView = UIStackView(arrangedSubviews: [dateLabel, titleLabel, placeLabel])
-        stackView.axis = .vertical
-        stackView.spacing = 4
+        
+        
+        let locationStackView = UIStackView(arrangedSubviews: [locationImageView, locationLabel])
+        
+        locationStackView.axis = .horizontal
+        locationStackView.spacing = 4
         
         contentView.addSubview(eventImageView)
-        contentView.addSubview(stackView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(locationStackView)
         contentView.addSubview(bookmarkButton)
         
         eventImageView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        locationStackView.translatesAutoresizingMaskIntoConstraints = false
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
         bookmarkButton.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            eventImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            eventImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            eventImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            eventImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            eventImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
             eventImageView.widthAnchor.constraint(equalToConstant: 100),
+            eventImageView.heightAnchor.constraint(equalToConstant: 120),
+
             
-            stackView.leadingAnchor.constraint(equalTo: eventImageView.trailingAnchor, constant: 8),
-            stackView.trailingAnchor.constraint(equalTo: bookmarkButton.leadingAnchor, constant: -8),
-            stackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            dateLabel.topAnchor.constraint(equalTo: eventImageView.topAnchor, constant: 5),
+            dateLabel.leadingAnchor.constraint(equalTo: eventImageView.trailingAnchor, constant: 10),
+            
+            titleLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: dateLabel.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             
             bookmarkButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            bookmarkButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+            bookmarkButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            bookmarkButton.widthAnchor.constraint(equalToConstant: 20),
+            bookmarkButton.heightAnchor.constraint(equalToConstant: 20),
+
+            
+            locationImageView.widthAnchor.constraint(equalToConstant: 14),
+            locationImageView.heightAnchor.constraint(equalToConstant: 14),
+            locationStackView.bottomAnchor.constraint(equalTo: eventImageView.bottomAnchor, constant: -5),
+            locationStackView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 5),
+ 
         ])
     }
     
     @objc private func bookmarkTapped() {
         isBookmarked.toggle()
-        bookmarkButton.tintColor = isBookmarked ? .red : .systemBlue
+        bookmarkButton.tintColor = isBookmarked ? .red : .blue
     }
     
     func configure(with event: Event) {
         dateLabel.text = event.date
         titleLabel.text = event.title
-        placeLabel.text = event.location
+        locationLabel.text = event.location
         eventImageView.image = event.image
-        
-        // Здесь нужно загрузить изображение мероприятия (например, из URL)
-        // eventImageView.image = ...
+
     }
 }
