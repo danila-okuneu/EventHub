4a94e84f8ea46e86bdb94a3621ebe324f1faefbf
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-11-21 22:30:52 +0500

    added Login and SignUp screens

--- EventHub/LoginScreen/LoginViewController.swift
+++ EventHub/LoginScreen/LoginViewController.swift
@@ -8,10 +8,203 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-	
-	override func viewDidLoad() {
-		
-	
-	}
-	
+
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "group")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "EventHub"
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let signInLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign in"
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let emailTextField = CustomTextField(
+        type: .email,
+        placeholderText: "abc@email.com",
+        icon: UIImage(systemName: "envelope")
+    )
+    
+    private let passwordTextField = CustomTextField(
+        type: .password,
+        placeholderText: "Your password",
+        icon: UIImage(systemName: "lock")
+    )
+    
+    private let rememberMeSwitch: UISwitch = {
+        let switchControl = UISwitch()
+        switchControl.onTintColor = UIColor.appPurpleDark
+        switchControl.thumbTintColor = .white
+        switchControl.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        return switchControl
+    }()
+    
+    private let rememberMeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Remember Me"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Forgot Password?", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        return button
+    }()
+    
+    private let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SIGN IN", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.appPurple
+        button.layer.cornerRadius = 12
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return button
+    }()
+    
+    private let signInButtonImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "buttonArrow")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let orLabel: UILabel = {
+        let label = UILabel()
+        label.text = "OR"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let loginWithGoogleButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .white
+        config.baseForegroundColor = .black
+        config.image = UIImage(named: "google")?.withRenderingMode(.alwaysOriginal)
+        config.imagePadding = 37
+        config.imagePlacement = .leading
+        config.title = "Login with Google"
+        
+        button.configuration = config
+        button.layer.cornerRadius = 12
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    private let signUpLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Don’t have an account?"
+        label.font = UIFont.systemFont(ofSize: 15)
+        return label
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign up", for: .normal)
+        button.setTitleColor(.appPurple, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .appGray
+        setupUI()
+        
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
+    
+    private func setupUI() {
+        [logoImageView, titleLabel, signInLabel, emailTextField, passwordTextField, rememberMeSwitch, rememberMeLabel, forgotPasswordButton, signInButton, signInButtonImageView, orLabel, loginWithGoogleButton, signUpLabel, signUpButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 31),
+            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 85),
+            logoImageView.widthAnchor.constraint(equalToConstant: 55),
+            logoImageView.heightAnchor.constraint(equalToConstant: 58),
+            
+            titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 8),
+            titleLabel.centerXAnchor.constraint(equalTo: logoImageView.centerXAnchor),
+            
+            signInLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 29),
+            signInLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 29),
+            
+
+            emailTextField.topAnchor.constraint(equalTo: signInLabel.bottomAnchor, constant: 22),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            emailTextField.heightAnchor.constraint(equalToConstant: 56),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 56),
+            
+            rememberMeSwitch.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 22),
+            rememberMeSwitch.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor),
+            
+            rememberMeLabel.centerYAnchor.constraint(equalTo: rememberMeSwitch.centerYAnchor),
+            rememberMeLabel.leadingAnchor.constraint(equalTo: rememberMeSwitch.trailingAnchor, constant: 20),
+            
+            forgotPasswordButton.centerYAnchor.constraint(equalTo: rememberMeSwitch.centerYAnchor),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            
+            signInButton.topAnchor.constraint(equalTo: rememberMeSwitch.bottomAnchor, constant: 30),
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.widthAnchor.constraint(equalToConstant: 271),
+            signInButton.heightAnchor.constraint(equalToConstant: 58),
+            
+            signInButtonImageView.trailingAnchor.constraint(equalTo: signInButton.trailingAnchor, constant: -14),
+            signInButtonImageView.centerYAnchor.constraint(equalTo: signInButton.centerYAnchor),
+            signInButtonImageView.widthAnchor.constraint(equalToConstant: 30),
+            signInButtonImageView.heightAnchor.constraint(equalToConstant: 30),
+            
+            orLabel.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 30),
+            orLabel.centerXAnchor.constraint(equalTo: signInButton.centerXAnchor),
+            
+            loginWithGoogleButton.topAnchor.constraint(equalTo: orLabel.bottomAnchor, constant: 25),
+            loginWithGoogleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginWithGoogleButton.widthAnchor.constraint(equalToConstant: 273),
+            loginWithGoogleButton.heightAnchor.constraint(equalToConstant: 56),
+            
+            signUpLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -41),
+            signUpLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -20),
+            
+            signUpButton.leadingAnchor.constraint(equalTo: signUpLabel.trailingAnchor, constant: 5),
+            signUpButton.centerYAnchor.constraint(equalTo: signUpLabel.centerYAnchor),
+        ])
+    }
+    
+    @objc private func signUpButtonTapped() {
+        let signupVC = SignupViewController()
+        signupVC.modalPresentationStyle = .fullScreen
+        present(signupVC, animated: true, completion: nil)
+    }
+}
+
+
+@available(iOS 17.0, *)
+#Preview{
+    return LoginViewController()
 }
