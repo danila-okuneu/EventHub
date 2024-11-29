fd0a7c95710adc0cd317cfd7737b963d9005acf8
Igor Guryan <sacujo93@yandex.ru>
2024-11-29 21:11:27 +0300

    fix

--- EventHub/App/SceneDelegate.swift
+++ EventHub/App/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
 		window?.backgroundColor = .appPurpleDark
 		window?.windowScene = windowScene
-        window?.rootViewController = ExploreViewController()
+        window?.rootViewController = CustomTabBarController()
 		window?.makeKeyAndVisible()
 	}
 
